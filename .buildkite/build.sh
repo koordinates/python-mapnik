eb32998e7e6c005db5749d31b2b0c6a792024b29
Craig de Stigter <craig@destigter.nz>
2021-03-22 11:49:41 +1300

    Buildkite CI for bionic

---
+++ .buildkite/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -eu
+
+if [ -n "${KX_BUILD_DEBUG-}" ]; then
+  echo "Enabling script debugging..."
+  set -x
+fi
+
+export TIMEFORMAT='🕑 %1lR'
+export DEBEMAIL=support@koordinates.com
+export DEBFULLNAME="Koordinates CI Builder"
+
+echo "Updating changelog..."
+
+DEB_BASE_VERSION=1:3.0.23
+DEB_VERSION="${DEB_BASE_VERSION}+ci${BUILDKITE_BUILD_NUMBER}-$(git show -s --date=format:%Y%m%d --format=git%cd.%h)"
+echo "Debian Package Version: ${DEB_VERSION}"
+
+if [ -n "${BUILDKITE_AGENT_ACCESS_TOKEN-}" ] ; then 
+  buildkite-agent meta-data set deb-base-version "$DEB_BASE_VERSION"
+  buildkite-agent meta-data set deb-version "$DEB_VERSION"
+
+  echo -e ":debian: Package Version: \`${DEB_VERSION}\`" \
+      | buildkite-agent annotate --style info --context deb-version
+fi
+
+time docker run \
+  -v "$(pwd):/src" \
+  -w "/src" \
+  -e DEBEMAIL \
+  -e DEBFULLNAME \
+  "${ECR}/ci-tools:latest" \
+    dch --distribution bionic --newversion "${DEB_VERSION}" "Koordinates CI build of ${BUILDKITE_COMMIT}: branch=${BUILDKITE_BRANCH} tag=${BUILDKITE_TAG-}"
+
+echo "--- Building debian package ..."
+# Uses a docker volume for ccache
+time docker run \
+  --rm \
+  -v "$(pwd):/kx/source" \
+  -v "ccache:/ccache" \
+  -e CCACHE_DIR=/ccache \
+  -w "/kx/source" \
+  "${ECR}/bionicbuild:latest" \
+    /kx/buildscripts/build_binary_package.sh -uc -us
+
+echo "--- Signing debian archives ..."
+time docker run \
+  -v "$(pwd):/src" \
+  -e "GPG_KEY=${APT_GPG_KEY}" \
+  -w "/src" \
+  "${ECR}/ci-tools:latest" \
+    sign-debs "/src/build-bionic/*.deb"

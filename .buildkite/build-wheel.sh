8e7ef9e2908f8c87ffbb73e291332f00af032a12
Craig de Stigter <craig@destigter.nz>
2023-10-09 13:28:43 +1300

    Build with cairo context support

--- .buildkite/build-wheel.sh
+++ .buildkite/build-wheel.sh
@@ -4,7 +4,9 @@ apt-get update -q
 apt-get install -q -y \
     libboost-python-dev \
     libmapnik3.1 \
-    libmapnik-dev
+    libmapnik-dev \
+    libcairo2-dev \
+    python3-cairo-dev
 
 export SKBUILD_CMAKE_VERBOSE=true
 export SKBUILD_LOGGING_LEVEL=INFO
@@ -12,7 +14,7 @@ export SKBUILD_CMAKE_BUILD_TYPE=RelWithDebInfo
 
 # not really needed here, but useful to know about
 export SKBUILD_BUILD_DIR=build
-export SKBUILD_CMAKE_DEFINE=CMAKE_CXX_STANDARD=14  # macOS
+export SKBUILD_CMAKE_DEFINE=CMAKE_CXX_STANDARD=14 # macOS
 
 # needs to be a X.Y.Z.P version for CMake
 COMMIT_TIME=$(git show -s --pretty=format:"%cd" --date=format:%Y%m%d%H%M%S)
